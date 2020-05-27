1a00886dc46769ad49bd86b66fec4882a5b6f095
roblav-sp <roberto.lavoie@gmail.com>
2020-05-26 22:02:37 -0300

    Git ignore for VS

---
+++ Assets/SCRIPTS/enemymove.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemymove : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public bool follow = false;
+    public float distance;
+    public Transform Hero;
+    public bool face = true;
+    public Animator enemy;
+
+
+    void Start()
+    {
+        enemy = GetComponent<Animator>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector2.Distance(this.transform.position, Hero.transform.position);
+        if ((Hero.transform.position.x >this.transform.position.x) && face)
+        {
+            Flip();
+        }
+        else if((Hero.transform.position.x< this.transform.position.x)&& !face)
+        {
+            Flip();
+        }
+        if ((follow)&& distance >1.6f)
+        {
+            if(Hero.transform.position.x < this.transform.position.x)
+            {
+                transform.Translate(new Vector2 (-speed * Time.deltaTime, 0));
+                enemy.SetBool("enemywalk", true);
+                enemy.SetBool("enemyidle", false);
+            }
+            else if(Hero.transform.position.x > this.transform.position.x)
+            {
+                transform.Translate(new Vector2( speed * Time.deltaTime, 0));
+                enemy.SetBool("enemywalk", true);
+                enemy.SetBool("enemyidle", false);
+            }
+
+        }
+        else
+        {   
+            // TODO FUTURE
+            enemy.SetBool("enemywalk", false);
+            enemy.SetBool("enemyidle", true);
+        }
+    }
+    void Flip()
+    {
+        face = !face;
+        Vector3 scala = this.transform.localScale;
+        scala.x *= -1;
+        this.transform.localScale = scala;
+    }
+    void OnTriggerEnter2D(Collider2D followit)
+    {
+        if (followit.gameObject.CompareTag("player"))
+        {
+            print("enemy attack");
+            follow = true;
+        }
+    }
+}
---
+++ Assets/SCRIPTS/playerMove.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerMove : MonoBehaviour
+{
+    public bool face = true;
+    public Transform HeroiT;
+    public float speed = 2.5f;
+    public float force = 40.0f;
+    public Rigidbody2D heroiRB;
+
+    public Text txtmessage;
+
+    // To jump
+    public bool liberaPulo = false;
+    public Transform check;
+    public LayerMask wtground;
+    public float radius = 0.3f;
+
+    public bool attackmove = false;
+    public Animator anim;
+    public bool vivo = true;
+    void Start()
+    {
+        HeroiT = GetComponent<Transform>();
+        heroiRB = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        //check = GetComponent<Transform>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (vivo)
+        {
+           
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(new Vector2(speed * Time.deltaTime, 0));
+                anim.SetBool("idle", false);
+                anim.SetBool("walk", true);
+                print("walking right");
+                if (face)
+                {
+                    Flip();
+                }
+
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(new Vector2(-speed * Time.deltaTime, 0));
+                anim.SetBool("idle", false);
+                anim.SetBool("walk", true);
+                print("walking left");
+                if (!face)
+                {
+                    Flip();
+                }
+            }
+            else if (Input.GetKey(KeyCode.Mouse0))
+            {
+                anim.SetBool("idle", false);
+                anim.SetBool("attack", true);
+                attackmove = true;
+                print("ATTACK");
+                if (!liberaPulo)
+                {
+                    anim.SetBool("attack", false);
+                    anim.SetBool("jumpattack", true);
+                    print("JUMP ATTACK");
+                }
+                else
+                {
+                    anim.SetBool("jumpattack", false);
+                }
+            }
+            else
+            {
+                attackmove = false;
+                anim.SetBool("idle", true);
+                anim.SetBool("walk", false);
+                anim.SetBool("attack", false);
+                anim.SetBool("jumpattack", false);
+                print("idle");
+            }
+
+        }
+        if (vivo)
+        {
+            print(liberaPulo); 
+            if (Input.GetKeyDown(KeyCode.Space) && liberaPulo)
+            {
+                heroiRB.AddForce(new Vector2(0, force), ForceMode2D.Impulse);
+                anim.SetBool("jump", true);
+                anim.SetBool("idle", false);
+
+            }
+            else
+            {
+               // Estou no chao ?
+               liberaPulo = Physics2D.OverlapCircle(check.position, radius, wtground);
+               txtmessage.text = "Les Dominateurs";                    
+            }
+        }
+    }
+    void Flip()
+    {
+        face = !face;
+        Vector3 scala = HeroiT.localScale;
+        scala.x *= -1;
+        HeroiT.localScale = scala;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            // Removed bc using other form to do it see update main
+            //liberaPulo = true;
+            anim.SetBool("jump", false);
+            //anim.SetBool("Idle", true);
+
+        }
+
+    }
+
+    //private void OnCollisionExit2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.CompareTag("ground"))
+    //    {
+    //        liberaPulo = false;
+    //        // anim.SetBool("Idle", true);
+    //
+    //    }
+    //}
+    //
+   // void OnTriggerEnter2D(Collider2D collision)
+   // {
+   //     if (collision.gameObject.CompareTag("enemy"))
+   //     {
+   //         print("collide");
+   //         vivo = false;
+   //         anim.SetBool("walk", false);
+   //         anim.SetBool("die", true);
+   //     }
+   // }
+}
